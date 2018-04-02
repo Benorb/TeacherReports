b519471db679df669f64862dcbbe87c4594d636a
Benorb <benorb@welldonw-software.com>
2018-04-02 11:34:54 +0300

    Mock back

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-// const delay = (data, millis = 100) =>
-//     new Promise(resolve => setTimeout(() => resolve(data), 1000))
+const delay = (data, millis = 100) =>
+    new Promise(resolve => setTimeout(() => resolve(data), 1000))
 
 // const getTeachers =  () => delay(['Teacher 1', 'Teacher 2', 'Teacher 3']);
 // const getCourses =  () => delay(['Course 1', 'Course 2', 'Course 3']);
@@ -7,39 +7,40 @@
 // const getTasks =  () => delay(['Task 1', 'Task 2', 'Task 3']);
 // const getReports =  () => delay(['Report 1', 'Report 2', 'Report 3']);
 
-const api = (path, params) => $.get(`http://localhost:8090/reports/${path}`).then(
-    function(res) {
-        console.log(res)
-        return res.rows
-    }, function(err) {
-        console.log(err)
+// const api = (path, params) => $.get(`http://localhost:8090/reports/${path}`).then(
+//     function(res) {
+//         console.log(res)
+//         return res.rows
+//     }, function(err) {
+//         console.log(err)
+//     }
+// )
+// window.api = (path, params) => {
+//     return api(path, params);
+// };
+
+class api {
+
+    getTeachers() {
+        return delay(['Teacher 1', 'Teacher 2', 'Teacher 3']);
     }
-)
-window.api = (path, params) => {
-    return api(path, params);
-};
 
-// class api {
-//
-//     getTeachers() {
-//         return delay(['Teacher 1', 'Teacher 2', 'Teacher 3']);
-//     }
-//
-//     getCourses(teacherId) {
-//         return delay(['Course 1', 'Course 2', 'Course 3']);
-//     }
-//
-//     getActivities(courseId) {
-//         return delay(['Activity 1', 'Activity 2', 'Activity 3']);
-//     }
-//
-//     getTasks(activityId) {
-//         return delay(['Task 1', 'Task 2', 'Task 3']);
-//     }
-//
-//     getReports() {
-//         return delay(['Report 1', 'Report 2', 'Report 3']);
-//     }
-// }
+    getCourses(teacherId) {
+        return delay(['Course 1', 'Course 2', 'Course 3']);
+    }
 
+    getActivities(courseId) {
+        return delay(['Activity 1', 'Activity 2', 'Activity 3']);
+    }
+
+    getTasks(activityId) {
+        return delay(['Task 1', 'Task 2', 'Task 3']);
+    }
+
+    getReports() {
+        return delay(['Report 1', 'Report 2', 'Report 3']);
+    }
+}
+
+window.api = new api('test');
 
