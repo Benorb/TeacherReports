238f07ad392599b64a761bd98090801a5333ad4c
Benorb <benorb@welldonw-software.com>
2018-04-03 03:05:55 +0300

    data from server + fix ui

--- js/api.js
+++ js/api.js
@@ -1,46 +1,24 @@
-const delay = (data, millis = 100) =>
-    new Promise(resolve => setTimeout(() => resolve(data), 1000))
-
-// const getTeachers =  () => delay(['Teacher 1', 'Teacher 2', 'Teacher 3']);
-// const getCourses =  () => delay(['Course 1', 'Course 2', 'Course 3']);
-// const getActivities =  () => delay(['Activity 1', 'Activity 2', 'Activity 3']);
-// const getTasks =  () => delay(['Task 1', 'Task 2', 'Task 3']);
-// const getReports =  () => delay(['Report 1', 'Report 2', 'Report 3']);
-
-// const api = (path, params) => $.get(`http://localhost:8090/reports/${path}`).then(
-//     function(res) {
-//         console.log(res)
-//         return res.rows
-//     }, function(err) {
-//         console.log(err)
-//     }
-// )
-// window.api = (path, params) => {
-//     return api(path, params);
-// };
-
-class api {
-
-    getTeachers() {
-        return delay(['Teacher 1', 'Teacher 2', 'Teacher 3']);
+const api = (path, params) => $.get(`http://localhost:8090/reports/${path}`).then(
+    function(res) {
+        console.log(res)
+        return res.rows
+    }, function(err) {
+        console.log(err)
     }
+)
+window.getTeacherCourses = (teacherId) => {
+    return api(`teachers/${teacherId}/courses`);
+};
 
-    getCourses(teacherId) {
-        return delay(['Course 1', 'Course 2', 'Course 3']);
-    }
+window.getCourseActivities = (courseId) => {
+    return api(`courses/${courseId}/activities`);
+};
 
-    getActivities(courseId) {
-        return delay(['Activity 1', 'Activity 2', 'Activity 3']);
-    }
+window.getActivityStages = (activityId) => {
+    return api(`activities/${activityId}/stages`);
+};
 
-    getTasks(activityId) {
-        return delay(['Task 1', 'Task 2', 'Task 3']);
-    }
-
-    getReports() {
-        return delay(['Report 1', 'Report 2', 'Report 3']);
-    }
-}
-
-window.api = new api('test');
+window.getReport = (reportName, teacherId, stageId) => {
+    return api(`${reportName}/teachers/${teacherId}/stages/${stageId}`);
+};
 
--- js/main.js
+++ js/main.js
@@ -1,85 +1,169 @@
-const _api = window.api;
-
-// specify the columns
-var columnDefs = [
-    { headerName: "Make", field: "make" },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" }
-];
-
-// specify the data
-var rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
-];
-
-// let the grid know which columns and what data to use
-var gridOptions = {
-    columnDefs: columnDefs,
-    rowData: rowData,
-    onGridReady: function (params) {
-        params.api.sizeColumnsToFit();
-    }
-};
-
-$(document).ready(function () {
-    $('.detailsDropdown').select2();
-});
-
-
-
 document.addEventListener("DOMContentLoaded", async () => {
-    let vm = new Vue({
-        el: '#app',
-        data: {
-            details: {
-                teacher: [],
-                course: [],
-                activity: [],
-                task: [],
-                report: [],
+    const getTeacherCourses = window.getTeacherCourses;
+    const getCourseActivities = window.getCourseActivities;
+    const getActivityStages = window.getActivityStages;
+    const getReport = window.getReport;
+
+    setSelect2()
+
+    var gridOptions = {
+        columnDefs: [],
+        rowData: [],
+        onGridReady: function (params) {
+            params.api.sizeColumnsToFit();
+        }
+    };
+
+    const queryParams = getUrlVars()
+    console.log(queryParams)
+    const teacherId = 2; //TODO: get from url
+
+    initiallizeGrid(gridOptions)
+
+    fillData(getTeacherCourses, 'coursesSelect', teacherId, 'Select Course');
+
+    window.selectChange = async function selectChange(e) {
+        var nextSelectId = null
+        var getData = null
+        var defaultText = 'Select Course'
+        switch(e.id) {
+            case 'coursesSelect': {
+                nextSelectId = 'activitySelect'
+                getData = getCourseActivities
+                defaultText = 'Select Activity'
             }
-        },
-        methods: {
-            fetchCourses: async (event) => {
-                const courses = await _api.getCourses();
-                vm.details.course = courses;
-            },
-            fetchActivities: async (event) => {
-                const activities = await _api.getActivities();
-                vm.details.course = activities;
-            },
-            fetchTasks: async (event) => {
-                const tasks = await _api.getTasks();
-                vm.details.course = tasks;
-            },
-            onSelectChange: async () => {
-                console.log(123)
-            },
-            chooseClicked: async () => {
-                console.log(123123123123)
+            break;
+            case 'activitySelect': {
+                nextSelectId = 'stageSelect'
+                getData = getActivityStages
+                defaultText = 'Select Stage'
             }
-        },
+            break;
+        }
 
-    });
+        if(nextSelectId) {
+            const selects = $('.dynamicSelect').toArray()
+            const selectedIndex = selects.findIndex((el) => el === e)
+            $(`#${nextSelectId}`).prop('disabled', false);
+            for (var i = selectedIndex+1; i < selects.length; i++) {
+                if(i === selectedIndex+1){
+                    fillData(getData, selects[i].id, e.value, defaultText)
+                } else {
+                    $(`#${selects[i].id}`).html('').prop('disabled', true);
+                }
+            }
+        }
+        checkData()
+    }
 
+    async function fillData(getData, selectId, entityId, defaultText){
+        const data = await getData(entityId)
+        $(`#${selectId}`).html('');
+        if(data){
+            $(`#${selectId}`).append('<option value="" disabled selected>' + defaultText + '</option>');
+            data.forEach((value) => {
+                $(`#${selectId}`).append('<option value="' + value.id + '">' + value.name + '</option>');
+            });
+        }
+    }
 
-    // lookup the container we want the Grid to use
-    const eGridDiv = document.querySelector('#myGrid');
+    window.submit = async function submit() {
+        const teacherId = 2;
+        const stageId = $('#stageSelect').val();
+        const reportName = $('#reportSelect').val();
+        const data = await getReport(reportName, teacherId, stageId)
+        if(data.length){
+            $('#error').css("visibility", "hidden");
+        } else {
+            $('#error').css("visibility", "visible");
+        }
+        fillTable(data)
+    }
 
-    // create the grid passing in the div to use together with the columns & data we want to use
-    new agGrid.Grid(eGridDiv, gridOptions);
+    function initiallizeGrid(gridOptions){
+        const eGridDiv = document.querySelector('#myGrid');
+        new agGrid.Grid(eGridDiv, gridOptions);
+    }
 
-    // const a = await _api('teachers/2/courses', null)
-    // console.log('a',a)
+    function fillTable(data){
+        var columnDefs = [];
+        const uniqArr = _.uniqBy(data, 'description')
+        if(uniqArr.length){
+            columnDefs = [
+                ...columnDefs,
+                { headerName: 'User Name', field: 'user_name' }
+            ]
+        }
+        var fieldNames = ['user_name']
+        uniqArr.map(item => {
+            fieldNames = [
+                ...fieldNames,
+                item.description
+            ]
+            return columnDefs = [
+                ...columnDefs,
+                {headerName: item.description, field: item.description}
+            ];
+        })
 
-    const teachers = await _api.getTeachers();
-    const reports = await _api.getReports();
-    vm.details.teacher = teachers;
-    vm.details.report = reports;
+        var groupData = _.groupBy(data,'username')
 
+        var rowData = []
+        _.map(groupData, (items) => {
+            let rowObj = {user_name: items[0].username}
+            items.map((item) => {
+                rowObj = {...rowObj, [item.description]: item.result || '-'}
+            })
+            rowData = [...rowData, rowObj]
+        })
+        gridOptions.api.setColumnDefs(columnDefs);
+        gridOptions.api.setRowData(rowData);
+        gridOptions.api.sizeColumnsToFit();
+    }
 
-});
+    function getUrlVars() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
 
+    function setSelect2() {
+        $('#reportSelect').select2({
+            dropdownAutoWidth : true,
+            // width: 'auto',
+        })
+
+        $('#coursesSelect').select2({
+            dropdownAutoWidth : true,
+            // width: 'auto',
+            placeholder:'Select Course'
+        })
+
+        $('#activitySelect').select2({
+            dropdownAutoWidth : true,
+            // width: 'auto',
+            placeholder:'Select Activity'
+        })
+
+        $('#stageSelect').select2({
+            dropdownAutoWidth : true,
+            // width: 'auto',
+            placeholder:'Select Stage'
+        })
+    }
+
+    function checkData() {
+        if($('#coursesSelect').val() && $('#activitySelect').val() && $('#stageSelect').val()){
+            $("#submit").prop('disabled', false).removeClass('disabled').addClass('enabled');
+        } else {
+            $("#submit").prop('disabled', true).removeClass('enabled').addClass('disabled');
+        }
+    }
+})
 
